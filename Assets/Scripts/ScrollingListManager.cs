212a06ac8ca6861c62cd2cc7315e83d507dd570a
Mateusz <mateusz.glinkowski@pwr.edu.pl>
2023-02-24 18:35:41 +0100

    [Script] Instantiating new object every time we refresh the list.

--- Assets/Scripts/ScrollingListManager.cs
+++ Assets/Scripts/ScrollingListManager.cs
@@ -1,44 +1,61 @@
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ScrollingListManager : MonoBehaviour
 {
-    public List<ItemData> ItemsData = new List<ItemData>();
     public ScrollRect ScrollRect;
     public GameObject ItemPrefab;
-    
+
     [SerializeField]
     private bool m_loadUsingAddressables;
-
-    private readonly ContentData m_contentData = new ContentData("Assets/Content/", "Content", "item");
+    
+    private List<Item> scrollList = new List<Item>();
 
     private ILoad Loader => m_loadUsingAddressables
         ? new LoadByAddressable(m_contentData.Label)
         : new LoadByDatabase(m_contentData.Path);
-    
+
     private IRefresh Refresher => m_loadUsingAddressables
         ? new RefreshByAddressable(m_contentData)
         : new RefreshByDatabase();
 
-    private Item m_item;
+    private readonly ContentData m_contentData = new ContentData("Assets/Content/", "Content", "item");
+
     private async void Start()
     {
+        var itemsData = await Refresher.Refresh(Loader);
 
-        ItemsData = await Refresher.Refresh(Loader);
-        
-        var itemGameObject = Instantiate(ItemPrefab, ScrollRect.content.transform);
-        m_item = itemGameObject.GetComponent<Item>();
-      
-        m_item.AssignData(ItemsData.Last());
+        foreach (var itemData in itemsData)
+        {
+            var itemGameObject = Instantiate(ItemPrefab, ScrollRect.content.transform);
+            var item = itemGameObject.GetComponent<Item>();
+            item.AssignData(itemData);
+            
+            scrollList.Add(item);
+        }
     }
 
     public void Refresh() => RefreshAsync();
 
     private async void RefreshAsync()
     {
-        ItemsData = await Refresher.Refresh(Loader);
-        m_item.AssignData(ItemsData.Last());
+         var itemsData = await Refresher.Refresh(Loader);
+
+         foreach (var item in scrollList)
+         {
+             Destroy(item.gameObject);
+         }
+         scrollList.Clear();
+         
+         
+         foreach (var itemData in itemsData)
+         {
+             var itemGameObject = Instantiate(ItemPrefab, ScrollRect.content.transform); 
+             var item = itemGameObject.GetComponent<Item>(); 
+             item.AssignData(itemData);
+             
+             scrollList.Add(item);
+         }
     }
 }
